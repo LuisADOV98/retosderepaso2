6f2446f65aba0c9d3e29693cc65f46ed066b0460
LuisADOV98 <luisadov26@gmail.com>
2023-07-24 11:36:08 +0200

    reto 3 completo

--- retos2/mobileProject/mobileCollection.ts
+++ retos2/mobileProject/mobileCollection.ts
@@ -4,26 +4,37 @@ import { Mobile } from "./mobile";
 export class MobileCollection{
    private mobiles: Mobile[]
    private totalPrice: number
-        constructor(mobiles: Mobile[], totalPriceCalculation: number){
+        constructor(mobiles: Mobile[]){
             this.mobiles = mobiles
-            this.totalPrice 
+            this.totalPrice = this.totalPriceCalculation()
         }
     
-    getMobiles():Mobile[]{
+    public getMobiles():Mobile[]{
         return this.mobiles
     }
-    setMobiles(newMobiles):void{
+    public setMobiles(newMobiles):void{
         this.mobiles = newMobiles
     }
-    getTotalPrice():number{
+    public getTotalPrice():number{
         return this.totalPrice
     }
-    setTotalPrice(newTotalPrice){
+    public setTotalPrice(newTotalPrice){
         this.totalPrice = newTotalPrice
     }
-    printCollection():Mobile[]{
-        return this.mobiles
-    
+    public printCollection():string{
+        let todos;
+        for (let i = 0; i < this.mobiles.length; i++) {
+            todos += this.mobiles[i].printMobile()
+        }
+            return todos
+        }
+
+    public totalPriceCalculation(){
+        let total:number = 0
+        for (let i = 0; i < this.mobiles.length; i++) {
+        total += this.mobiles[i].getPrice();
+        }
+        return total
     }
 
     }
