3a95023cff27d4c918714f68c503311abd03bfca
LuisADOV98 <luisadov26@gmail.com>
2023-07-24 18:00:55 +0200

    ultima final

--- retos4/algebraProject/point.ts
+++ retos4/algebraProject/point.ts
@@ -9,43 +9,6 @@ export class Point{
       this.y = y
   }
 
-<<<<<<< HEAD
-    public getX():number{
-        return this.x
-    }
-    public setX(newX):void{
-        this.x = newX
-    }
-    public getY():number{
-        return this.y
-    }
-    public setY(newY):void{
-        this.y = newY
-    }
-    public toString(){
-        return `${this.getX}, ${this.getY}`
-    }
-    public distanceToOrigin():number{
-        return Math.round(Math.sqrt(this.getX() * this.getX() + this.getY() * this.getY()))
-    }
-    public calcularDistancia(anotherPoint: Point):number{
-        let w = this.getX() - anotherPoint.getX();
-        let z = this.getY() - anotherPoint.getY();
-        return Math.sqrt(w * w + z * z);
-    }
-    calculateQuadrant():number{
-            let resultado;
-            if (this.x === 0 || this.y === 0) {
-              resultado = 0;
-            } else if (this.getX() > 0 && this.getY() > 0) {
-              resultado = 1;
-            } else if (this.getX() < 0 && this.getY() > 0) {
-              resultado = 2;
-            } else if (this.getX() < 0 && this.getY() < 0) {
-              resultado = 3;
-            } else {
-              resultado = 4;
-=======
   public getX():number{
       return this.x
   }
@@ -93,9 +56,7 @@ export class Point{
             if (currentDistance < minDistance) {
               minDistance = currentDistance;
               nearestPoint = points[i];
->>>>>>> repaso4
             }
-            return resultado
           }
       
           return nearestPoint;
